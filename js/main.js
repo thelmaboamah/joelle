f91220873ddc6c42e6f039259c74376f0f8ab66d
Thelma Boamah <thelma.a.boamah@gmail.com>
2016-08-20 11:28:21 -0400

    click function for desktop nav

--- js/main.js
+++ js/main.js
@@ -1,23 +1,25 @@
 $(document).ready(function(){
-	
-	// $(window).resize()
-	var vw = $(window).width();
+		
+	//Mobile navigation
+	$('.fa-bars').click(function(){
+		$('#mobile-nav').fadeIn();
+		$('#nav_heading, main, footer').hide();
+	});
 
-	if(vw <= 768) {
-		//Mobile navigation
-		$('.fa-bars').click(function(){
-			$('nav').fadeIn();
-			$('#nav_heading, main, footer').hide();
-		});
-
-		$('.fa-times, .nav_item a').click(function(){
-			$('nav').hide();
-			$('#nav_heading, main, footer').show();
-		});	
-	} else {
-		$('nav').show();
-	};
+	$('.fa-times, .nav_item a').click(function(){
+		$('#mobile-nav').hide();
+		$('#nav_heading, main, footer').show();
+	});	
 
 
-	
+	//Desktop navigation
+	$('#desktop-nav a').each(function(){
+		var navId = $(this).attr('href');
+		$(this).click(function(){
+			$('#welcome').hide();
+			$(''+ navId).show();
+		});		
+	});
+
+
 });
