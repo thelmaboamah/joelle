937fd6feeda0c3e44f146c8786b596f3592b2d67
Thelma Boamah <thelma.a.boamah@gmail.com>
2016-08-20 02:56:42 -0400

    started mobile nav interaction

--- js/main.js
+++ js/main.js
@@ -1,3 +1,19 @@
 $(document).ready(function(){
-	console.log('ready!');
+	// var vw = $(window).width();
+
+	// if(vw <= 768) {
+	// 	console.log('tada!');
+	// }
+
+
+	//Mobile navigation
+	$('.fa-bars').click(function(){
+		$('nav').fadeIn();
+		$('#nav_heading, main, footer').hide();
+	});
+
+	$('.fa-times').click(function(){
+		$('nav').hide();
+		$('#nav_heading, main, footer').show();
+	});
 });
