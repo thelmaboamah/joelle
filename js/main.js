46d6e07e12e935f9b9be01364d5c0a8515372301
Thelma Boamah <thelma.a.boamah@gmail.com>
2016-08-21 12:07:55 -0400

    fixing navigation behavior

--- js/main.js
+++ js/main.js
@@ -1,36 +1,37 @@
 $(document).ready(function(){
-		
+
+	//Desktop navigation
+
+	function dNav() {
+		$('#desktop-nav a').each(function(){
+			var navId = $(this).attr('href');
+			$(this).click(function(){
+				$('#welcome, .main_content').hide();
+				$(''+ navId).show();
+			});		
+		});
+	}
+
+
 	//Mobile navigation
-	$('.fa-bars').click(function(){
-		$('#mobile-nav').fadeIn();
-		$('#nav_heading, main, footer').hide();
-	});
+	function mNav(){
+		$('.fa-bars').click(function(){
+			$('#mobile-nav').fadeIn();
+			$('#nav_heading, main, footer').hide();
+		});
 
-	$('.fa-times, .nav_item a').click(function(){
-		$('#mobile-nav').hide();
-		$('#nav_heading, main, footer').show();
-	});	
-
-
-	$(window).resize(function(){
-		var width = $(window).width();
-
-		if (width > 768) {
-			//Desktop navigation
-			$('#desktop-nav a').each(function(){
-				var navId = $(this).attr('href');
-				$(this).click(function(){
-					$('#welcome, .main_content').hide();
-					$(''+ navId).show();
-				});		
-			});
-		} else {
-			$('.main_content').show();
-		}
-
-	});
+		$('.fa-times, #mobile-nav a').click(function(){
+			$('#mobile-nav').hide();
+			$('#nav_heading, main, footer').show();
+		});	
+	}
 	
-	
+	dNav();
+	mNav();
 
+	//Dealing with wrong content showing upon window resizing
+	
+		// Need to add code that listens for when the page resizes and activates
+		// the correct navigation behavior
 
 });
